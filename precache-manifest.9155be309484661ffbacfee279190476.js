73ea0555d44e87494faa127bb25e3210dd1c8a39
Ismael Choque <ismadev2.7@gmail.com>
2020-06-05 10:28:39 -0400

    Updates

---
+++ precache-manifest.9155be309484661ffbacfee279190476.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b80623c3b4f06aaa4ffe53ebe68945",
+    "url": "/Login-ui/index.html"
+  },
+  {
+    "revision": "3305db61e3a8938cebce",
+    "url": "/Login-ui/static/css/main.3c076489.chunk.css"
+  },
+  {
+    "revision": "fc6b60d42c32eb649ffe",
+    "url": "/Login-ui/static/js/2.bfbb5ca0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Login-ui/static/js/2.bfbb5ca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3305db61e3a8938cebce",
+    "url": "/Login-ui/static/js/main.7d2a697a.chunk.js"
+  },
+  {
+    "revision": "6f9d4fe085c6a3bbd8b2",
+    "url": "/Login-ui/static/js/runtime-main.34dd8763.js"
+  },
+  {
+    "revision": "9ef697cfc0544f81ba1b09d808163c01",
+    "url": "/Login-ui/static/media/Wave.9ef697cf.png"
+  },
+  {
+    "revision": "5911f7233c89e52f6fd9f87c0ea68556",
+    "url": "/Login-ui/static/media/undraw_authentication_fsn5.5911f723.svg"
+  }
+]);
