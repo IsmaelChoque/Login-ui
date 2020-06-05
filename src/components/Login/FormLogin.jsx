197f48d927f471172cb546f9f8b23580fed55c61
Ismael Choque <ismadev2.7@gmail.com>
2020-06-05 10:30:59 -0400

    Prueba

--- src/components/Login/FormLogin.jsx
+++ src/components/Login/FormLogin.jsx
@@ -5,89 +5,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styled from 'styled-components'
 
-const LoginForm = styled.div`
-  background: var(--primary-gradient);
-  margin: 1rem;
-  padding: 2rem;
-  padding-left: 0;
-
-  .btn {
-    padding: .8rem 1.5rem;
-    border: none;
-    border-radius: 4rem;
-    background: #2B5876;
-    color: whitesmoke;
-    font-size: .7rem;
-    font-family: var(--Montserrat);
-    box-shadow: var(--primary-shadow);
-    float: right;
-    cursor: pointer;
-  }
-
-  form {
-    margin-top: 6.5rem;
-
-    .titles {
-      margin-bottom: 2.2rem;
-
-      h3, h6 {
-        font-family: var(--Montserrat);
-        padding: 0;
-        margin: 0;
-        padding-left: .5rem;
-      }
-
-      h6 {
-        padding-left: 4.5rem;
-        font-family: var(--Lobster);
-        color: #444444D2;
-      }
-    }
-
-    .form-group {
-      text-align: left;
-      border: 1px solid var(--border-color);
-      margin-top: 1.5rem;
-      margin-right: 0rem;
-      border-radius: 3rem;
-      width: 100%;
-      padding: .4rem;
-
-      .form-input {
-        padding: .5rem 1rem;
-        background: transparent;
-        border: none;
-        font-family: var(--Montserrat);
-        font-size: .9rem;
-        overflow: hidden;
-        width: calc(100% - 65px);
-
-      }
-
-      input:focus{
-        outline: none;
-      }
-
-      .input-icon {
-        display: inline;
-        color: var(--border-color);
-        margin-left: 4px;
-      }
-    }
-
-    .btn-login{
-      margin-top: 3rem;
-      padding: 1rem 0rem;
-      margin-left: 0rem;
-      width: 100%;
-      font-size: 1rem;
-    }
-  }
-` 
 
 const FormLogin = () => {
   return (
-    <LoginForm>
+    <div>
       <button className="btn btn-signup">Sign Up</button>
       <form>
         <div className="titles">
@@ -110,7 +31,7 @@ const FormLogin = () => {
 
         <button type="submit" className="btn btn-login">Login</button>
       </form>
-    </LoginForm>
+    </div>
   );
 }
  
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -9,79 +9,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from 'styled-components'
 import FormLogin from './FormLogin';
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  
-  .panel {
-    width: 70%;
-    height: 77%;
-    box-shadow: var(--primary-gradient);
 
-    .row {
-      display: grid;
-      grid-template-columns: 65% 35%;
-      height: 100%;
-      background: #FFF;
-      
-      .liquid {
-        background: url('${Wave}');
-        background-size: 100%;-webkit-background-size: 100%;
-        background-repeat: no-repeat;
-        h4 {
-          text-align: left;
-          padding: 0 2rem;
-          color: whitesmoke;
-          font-family: var(--Montserrat);
-          
-        }
-        .owl-carousel {
-          float: right;
-          width: 100%;
-          height: 80%;
-
-          img{
-            height: 100%; 
-            width: 100%;
-            background-repeat: no-repeat;
-            background-size: cover;
-            padding-right: 3rem;
-          }
-        }
-
-        .follow {
-          margin-top: 0;
-          padding: 0 1.4rem;
-          font-family: var(--Lobster);
-          color: whitesmoke;
-          position: relative;
-          font-size: .9rem;
-
-          ::after {
-            content: '';
-            position: absolute;
-            left: 73px;
-            width: 50px;
-            border: 1px solid whitesmoke;
-            border-radius: 4rem;
-            margin-top: .5rem;
-          }
-
-          svg:first-child{
-            margin-left: 3rem;
-          }
-          
-        }
-      }
-    }
-  }
-`
 
 const Login = () => {
   return (
-    <Container>
+    <div>
       <div className="panel">
         <div className="row">
           <div className="col liquid">
@@ -98,7 +30,7 @@ const Login = () => {
           <FormLogin/>
         </div>
       </div>
-    </Container>
+    </div>
   );
 }
  
